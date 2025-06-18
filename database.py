a41aa7482a4b1450358aa89e560920fea1b0076d
Lovish <lovish.d@kansoftware.com>
2025-06-18 14:06:31 +0530

    final

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
 # Load from env or use default
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/video_db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://video_db_nl3t_user:TrHs5IAE1s3ZLH51RD7m2uCrhsxytnjD@dpg-d197eevfte5s73c38icg-a.oregon-postgres.render.com/video_db_nl3t")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
