31d921baaae8dec971507dc35338e7a066ed2106
Lovish <lovish.d@kansoftware.com>
2025-06-19 17:48:57 +0530

    "final push"

--- video_data.py
+++ video_data.py
@@ -1,26 +1,26 @@
 # video_data.py (or place this at the top of your main file)
 sample_videos = [
     {
-        "avatar_id": 1,
+        "avatar_id": 3,
         "language": "English",
-        "video_type": "Report Explainer",
-        "disease": "Endometriosis",
-        "video_url": "https://datareel-eshre-backend-89ex.onrender.com/static/ehsre_demo.mp4"
+        "video_type": "Disease Explainer",
+        "disease": "Tubal Block",
+        "video_url": "https://datareel-eshre-backend-89ex.onrender.com/static/eshre_demo3.mp4"
     },
     {
         
-        "avatar_id": 2,
+        "avatar_id": 1,
         "language": "English",
-        "video_type": "Initial Consultation",
+        "video_type": "Disease Explainer",
         "disease": "Tubal Block",
         "video_url": "https://datareel-eshre-backend-89ex.onrender.com/static/eshre_demo2.mp4"
     },
 
 {
-      "avatar_id": 3,
+      "avatar_id": 2,
         "language": "English",
-        "video_type": "Initial Consultation",
+        "video_type": "Disease Explainer",
         "disease": "Tubal Block",
-        "video_url": "https://datareel-eshre-backend-89ex.onrender.com/static/eshre_demo3.mp4"
+        "video_url": "https://datareel-eshre-backend-89ex.onrender.com/static/ehsre_demo.mp4"
 }
 ]
