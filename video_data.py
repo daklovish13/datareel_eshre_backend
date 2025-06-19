d289a6a168cf459d5389fac48fb9ff80c233375b
Lovish <lovish.d@kansoftware.com>
2025-06-19 18:40:09 +0530

    "report explainer"

--- video_data.py
+++ video_data.py
@@ -150,6 +150,27 @@ sample_videos = [
         "video_type": "Disease Explainer",
         "disease": "Tubal Block",
         "video_url": "https://datareel-eshre-backend-89ex.onrender.com/static/eshre_demo3.mp4"
+    },
+    {
+        "avatar_id": 1,
+        "language": "English",
+        "video_type": "Report Explainer",
+        "disease": "Tubal Block",
+        "video_url": "https://datareel-eshre-backend-89ex.onrender.com/static/eshre_demo2.mp4"
+    },
+    {
+        "avatar_id": 2,
+        "language": "English",
+        "video_type": "Report Explainer",
+        "disease": "Tubal Block",
+        "video_url": "https://datareel-eshre-backend-89ex.onrender.com/static/ehsre_demo.mp4"
+    },
+    {
+        "avatar_id": 3,
+        "language": "English",
+        "video_type": "Report Explainer",  
+        "disease": "Tubal Block",
+        "video_url": "https://datareel-eshre-backend-89ex.onrender.com/static/eshre_demo3.mp4"
     }
 
 
