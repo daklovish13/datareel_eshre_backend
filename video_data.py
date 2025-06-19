50bdaab67fb7ea92510ee352a6861921064874e2
Lovish <lovish.d@kansoftware.com>
2025-06-19 18:06:32 +0530

    final push 2

--- video_data.py
+++ video_data.py
@@ -23,4 +23,64 @@ sample_videos = [
         "disease": "Tubal Block",
         "video_url": "https://datareel-eshre-backend-89ex.onrender.com/static/ehsre_demo.mp4"
 }
+,
+ {
+        "avatar_id": 3,
+        "language": "English",
+        "video_type": "Disease Explainer",
+        "disease": "PCOD",
+        "video_url": "https://datareel-eshre-backend-89ex.onrender.com/static/eshre_demo3.mp4"
+    },
+    {
+        
+        "avatar_id": 1,
+        "language": "English",
+        "video_type": "Disease Explainer",
+        "disease": "PCOD",
+        "video_url": "https://datareel-eshre-backend-89ex.onrender.com/static/eshre_demo2.mp4"
+    },
+
+{
+      "avatar_id": 2,
+        "language": "English",
+        "video_type": "Disease Explainer",
+        "disease": "PCOD",
+        "video_url": "https://datareel-eshre-backend-89ex.onrender.com/static/ehsre_demo.mp4"
+},
+
+
+{
+      "avatar_id": 2,
+        "language": "English",
+        "video_type": "Disease Explainer",
+        "disease": "Tubal Block",
+        "video_url": "https://datareel-eshre-backend-89ex.onrender.com/static/ehsre_demo.mp4"
+},
+
+ {
+        "avatar_id": 3,
+        "language": "English",
+        "video_type": "Teratozoospermia",
+        "disease": "PCOD",
+        "video_url": "https://datareel-eshre-backend-89ex.onrender.com/static/eshre_demo3.mp4"
+    },
+    {
+        
+        "avatar_id": 1,
+        "language": "English",
+        "video_type": "Teratozoospermia",
+        "disease": "PCOD",
+        "video_url": "https://datareel-eshre-backend-89ex.onrender.com/static/eshre_demo2.mp4"
+    },
+
+{
+      "avatar_id": 2,
+        "language": "English",
+        "video_type": "Disease Explainer",
+        "disease": "Teratozoospermia",
+        "video_url": "https://datareel-eshre-backend-89ex.onrender.com/static/ehsre_demo.mp4"
+}
+
+
+
 ]
